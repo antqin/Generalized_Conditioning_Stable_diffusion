73e0c6b7463f4d118bde3e9de620782f815761cd
Anthony Qin <antqin27@stanford.edu>
2024-06-03 23:50:21 -0700

    Add run inference script

--- run_lora_inference.py
+++ run_lora_inference.py
@@ -14,7 +14,7 @@ prompt = "generate a realistic interior room design of a bedroom"
 
 # Generate the image
 generator = torch.Generator(device="cuda").manual_seed(42)  # Set seed for reproducibility
-images = pipeline(prompt, num_inference_steps=25, generator=generator).images
+images = pipeline(prompt, num_inference_steps=10, generator=generator).images
 
 # Save the generated image
-images[0].save("generated_bedroom25.png")
+images[0].save("generated_image10.png")
