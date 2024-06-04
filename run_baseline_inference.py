0eb99524ec84a01fc9137fad302fb3c39ae5ab75
Anthony Qin <antqin27@stanford.edu>
2024-06-04 15:35:53 -0700

    script

--- run_baseline_inference.py
+++ run_baseline_inference.py
@@ -6,10 +6,10 @@ model_name_or_path = "stabilityai/stable-diffusion-2-1"
 pipeline = StableDiffusionPipeline.from_pretrained(model_name_or_path, torch_dtype=torch.float16).to("cuda")
 
 # Define the prompt for image generation
-prompt = "generate a realistic interior room design of a dining room"
+prompt = "generate a realistic interior room design of a living room"
 
 # Generate and save images with different numbers of inference steps for comparison
 for steps in [5, 10, 25, 50, 75, 100]:
     generator = torch.Generator(device="cuda").manual_seed(1337)  # Set seed for reproducibility
     images = pipeline(prompt, num_inference_steps=steps, generator=generator).images
-    images[0].save(f"./finetune-lora-dreambooth-output/baseline_dining_{steps}_steps.png")
+    images[0].save(f"./baseline_images/baseline_living_{steps}_steps.png")
