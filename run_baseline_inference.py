4ad8b510977b8dfbea18451116096ff1aa488699
Anthony Qin <antqin27@stanford.edu>
2024-06-04 00:14:29 -0700

    scripts

--- run_baseline_inference.py
+++ run_baseline_inference.py
@@ -5,15 +5,11 @@ from diffusers import StableDiffusionPipeline
 model_name_or_path = "stabilityai/stable-diffusion-2-1"
 pipeline = StableDiffusionPipeline.from_pretrained(model_name_or_path, torch_dtype=torch.float16).to("cuda")
 
-# Load the LoRA weights
-lora_weights_path = "./finetune-lora-output/checkpoint-900/pytorch_lora_weights.safetensors"
-pipeline.unet.load_attn_procs(lora_weights_path)
-
 # Define the prompt for image generation
-prompt = "generate a realistic interior room design of a living room"
+prompt = "generate a realistic interior room design"
 
-# Generate and save images with different numbers of inference steps
-for steps in [5, 10, 25, 50, 75, 100]:
+# Generate and save images with different numbers of inference steps for comparison
+for steps in [25, 50, 75, 100]:
     generator = torch.Generator(device="cuda").manual_seed(42)  # Set seed for reproducibility
     images = pipeline(prompt, num_inference_steps=steps, generator=generator).images
-    images[0].save(f"generated_living_{steps}_steps.png")
+    images[0].save(f"baseline_generated_image_{steps}_steps.png")
